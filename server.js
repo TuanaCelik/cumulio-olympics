41a03a0852ab4b715803334db41e09d03abc2c82
TuanaCelik <tuanacelik95@gmail.com>
2021-08-11 12:47:12 +0200

    new meta tags for og:image

--- server.js
+++ server.js
@@ -5,23 +5,6 @@ const { join } = require("path");
 
 const app = express();
 
-// const client = new Cumulio({
-//   api_key: process.env.CUMULIO_API_KEY,
-//   api_token: process.env.CUMULIO_API_TOKEN,
-// });
-
-// app.get("/authorization", (req, res) => {
-//   client
-//     .create("authorization", {
-//       type: "temporary",
-//       expiry: "1 day",
-//       inactivity_interval: "30 minutes",
-//     })
-//     .then((result) => {
-//       return res.status(200).json(result);
-//     });
-// });
-
 app.use(express.static(join(__dirname, "public")));
 
 app.get("/*", (req, res) => {
